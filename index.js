a81580b3d2898fc62ecab1ea438c4d7ed160e0d3
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-16 09:59:26 +0300

    Mocked response

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 exports.handler = (event, context, callback) => {
     try {
-        callback(null, 'Report extractor');
+        callback(null, [
+            {
+                id: 1,
+                createdAt: 1557989832,
+                dateFrom: 1557989832,
+                dateTo: 1557989832
+            },
+            {
+                id: 2,
+                createdAt: 1557989832,
+                dateFrom: 1557989832,
+                dateTo: 1557989832
+            },
+            {
+                id: 3,
+                createdAt: 1557989832,
+                dateFrom: 1557989832,
+                dateTo: 1557989832
+            }
+        ]);
     } catch (error) {
         console.log('Error occured', error);
 
